72711c4a8a31f805d0cb7133893ab2511626066e
Chelsey McKinney <chelseymckinney@Chelseys-MacBook-Pro.local>
2019-11-07 18:57:19 -0600

    text for recent work section

--- chels-portfolio/src/components/Backup.vue
+++ chels-portfolio/src/components/Backup.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="backup">
-    <hr/>
-    <h1>H1 Title</h1>
-    <h2>H2 Title</h2>
+    
+    <h1>Chelsey McKinney</h1>
+    <h2>Full Stack Developer</h2>
     <hr/>
         <div class="about">
-            <h3>Hello!This is an H3!</h3>
+            <h3>Hello! My name is Chelsey!</h3>
                 <figure>
                     <img src="http://placekitten.com/200/300" alt="Chelsey at an event" />
                     <figcaption>
@@ -21,45 +21,53 @@
                 <button>LinkedIn</button>
                 <button>Twitter</button>
         </div>
+       <hr/> 
         <div class="work">
-            <h3>My Work</h3>
+            <h3>Recent Work</h3>
         <!--will house written/coding projects--> 
         <div class="row">
   <div class="column">
     <div class="card">
-      <h3>Card 1</h3>
-      <p>Some text</p>
-      <p>Some text</p>
+      <h3>100 Days, Racism, and Starting Over</h3>
+      <p><a href="">article</a></p>
+      <p>What happens when a junior dev stops coding for 3 months? What really matters is what happens after the tech reprieve. Learn about how unplugging from technology allowed me to grow immensely, and how I regained my faith in programming by completing a coding challenge. Tech Talk given at Minnebar in April 2019. 
+</p>
     </div>
   </div> 
 
   <div class="column">
     <div class="card">
-      <h3>Card 2</h3>
-      <p>Some text</p>
-      <p>Some text</p>
+      <h3>Shit Black Parents Say Lorem Ipsum</h3>
+      <p><a href="https://secret-temple-73467.herokuapp.com/">website</a></p>
+      <p>A lorem ipsum generator I made as an homage to Black Parents sayings. 
+        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) 
+        This project was made using HTML5, CSS3, and ExpressJS. 
+      </p>
     </div>
   </div>
   
   <div class="column">
     <div class="card">
-      <h3>Card 3</h3>
-      <p>Some text</p>
-      <p>Some text</p>
+      <h3>My Love Of Semicolons</h3>
+      <p><a href="https://softwareforgood.com/my-love-of-semicolons/
+">article</a></p>
+      <p>Last summer, I worked as an intern for a company called Software for Good.  During my internship, I got the opportunity to write the "Theme Of The Week" for Software For Good's blog. The "Theme Of The Week," is when an employee from Software For Good takes time to write whats on their mind. For me, I wrote about my love of semicolons, and how programming has made me enjoy semicolons more.
+</p>
     </div>
   </div>
   
   <div class="column">
     <div class="card">
-      <h3>Card 4</h3>
-      <p>Some text</p>
-      <p>Some text</p>
+      <h3>Seshat Swap</h3>
+      <p><a href="https://agitated-swanson-0e797f.netlify.com/
+">website</a></p>
+      <p>An app made for a hackathon called Hack The Gap. 
+       “Seshat Swap,” is a community book sharing app that would promote authors of colors, while also sharing books with other users of color. App was made at all female hackathon, Hack The Gap in January 2019. Worked in a group with Lanice Sims, Jenessa White, and Shanise Barona. 
+       Made using React and react strap. </p>
     </div>
   </div> 
-
-</div>
-
-         
+</div>        
     </div>
 </div>
 
@@ -72,9 +80,14 @@
 
 
 <style>
-* {
+
+/* figure{
+  float:left;
+} */
+
+/* * {
   box-sizing: border-box;
-}
+} */
 
 body {
   font-family: Arial, Helvetica, sans-serif;
