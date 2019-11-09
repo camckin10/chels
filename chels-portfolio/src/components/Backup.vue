34848932be75c011ee2ce65a7d77a69d0f3f7465
Chelsey McKinney <chelseymckinney@Chelseys-MacBook-Pro.local>
2019-11-08 18:53:24 -0600

    adding bio img + updating portfolio section

--- chels-portfolio/src/components/Backup.vue
+++ chels-portfolio/src/components/Backup.vue
@@ -4,73 +4,75 @@
     <h1>Chelsey McKinney</h1>
     <h2>Full Stack Developer</h2>
     <hr/>
-        <div class="about">
+        <div id="about">
             <h3>Hello! My name is Chelsey!</h3>
-                <figure>
-                    <img src="http://placekitten.com/200/300" alt="Chelsey at an event" />
+                <figure style="float:left;">
+                    <img src="img/chelsey.jpg" alt="Chelsey at an event" />
                     <figcaption>
                     Photo Credit Jenessa White <a href="http://chelseymckinney.com">chelseymckinney.com</a>
                     </figcaption>
+                    <!--add social media icons here-->
                 </figure>
                 <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
                     sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    
                 </h4>
-                <button>Resume</button>
-                <button>LinkedIn</button>
-                <button>Twitter</button>
+              <button>Resume</button>
+              <button>LinkedIn</button>
+              <button>Twitter</button>  
         </div>
        <hr/> 
-        <div class="work">
-            <h3>Recent Work</h3>
-        <!--will house written/coding projects--> 
-        <div class="row">
-  <div class="column">
-    <div class="card">
-      <h3>100 Days, Racism, and Starting Over</h3>
-      <p><a href="">article</a></p>
-      <p>What happens when a junior dev stops coding for 3 months? What really matters is what happens after the tech reprieve. Learn about how unplugging from technology allowed me to grow immensely, and how I regained my faith in programming by completing a coding challenge. Tech Talk given at Minnebar in April 2019. 
-</p>
-    </div>
-  </div> 
-
-  <div class="column">
-    <div class="card">
-      <h3>Shit Black Parents Say Lorem Ipsum</h3>
-      <p><a href="https://secret-temple-73467.herokuapp.com/">website</a></p>
+        <div class="work" style="align-items:center;">
+             <h3>Recent Work</h3>
+             <div class="grid-container">
+  <div class="card">
+    <div class="bg-img"></div>
+    <div class="content">
+      <h4>100 Days, Racism, and Starting Over</h4>
+      <p>What happens when a junior dev stops coding for 3 months? 
+        What really matters is what happens after the tech reprieve. L
+        earn about how unplugging from technology allowed me to grow immensely, and how I regained my faith in programming by completing a coding challenge. 
+        Tech Talk given at Minnebar in April 2019. </p> 
+      <button><a href="">Read More</a></button> </div>
+  </div>
+  <div class="card">
+     <div class="bg-img"></div>
+    <div class="content">
+      <h4>Shit Black Parents Say Lorem Ipsum</h4>
       <p>A lorem ipsum generator I made as an homage to Black Parents sayings. 
         Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
         The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) 
-        This project was made using HTML5, CSS3, and ExpressJS. 
-      </p>
-    </div>
+        This project was made using HTML5, CSS3, and ExpressJS. </p> 
+      <button><a href="https://secret-temple-73467.herokuapp.com/">Readmore</a></button></div>
   </div>
-  
-  <div class="column">
-    <div class="card">
-      <h3>My Love Of Semicolons</h3>
-      <p><a href="https://softwareforgood.com/my-love-of-semicolons/
-">article</a></p>
-      <p>Last summer, I worked as an intern for a company called Software for Good.  During my internship, I got the opportunity to write the "Theme Of The Week" for Software For Good's blog. The "Theme Of The Week," is when an employee from Software For Good takes time to write whats on their mind. For me, I wrote about my love of semicolons, and how programming has made me enjoy semicolons more.
-</p>
-    </div>
+  <div class="card">
+     <div class="bg-img"></div>
+    <div class="content">
+      <h4>My Love Of Semicolons</h4>
+      <p>Last summer, I worked as an intern for a company called Software for Good.  
+        During my internship, I got the opportunity to write the "Theme Of The Week" for Software For Good's blog. 
+        The "Theme Of The Week," is when an employee from Software For Good takes time to write whats on their mind. 
+        For me, I wrote about my love of semicolons, and how programming has made me enjoy semicolons more.</p> 
+      <button><a href="https://softwareforgood.com/my-love-of-semicolons/">Readmore</a></button></div>
   </div>
-  
-  <div class="column">
-    <div class="card">
-      <h3>Seshat Swap</h3>
-      <p><a href="https://agitated-swanson-0e797f.netlify.com/
-">website</a></p>
+  <div class="card">
+     <div class="bg-img"></div>
+    <div class="content">
+      <h4>Seshat Swap</h4>
       <p>An app made for a hackathon called Hack The Gap. 
        “Seshat Swap,” is a community book sharing app that would promote authors of colors, while also sharing books with other users of color. App was made at all female hackathon, Hack The Gap in January 2019. Worked in a group with Lanice Sims, Jenessa White, and Shanise Barona. 
-       Made using React and react strap. </p>
-    </div>
-  </div> 
-</div>        
-    </div>
+       Made using React and react strap.</p> 
+      <button><a href="https://agitated-swanson-0e797f.netlify.com/">Readmore</a></button></div>
+  </div>
 </div>
-
+       
+</div>
+</div>
 
 </template>
 
@@ -80,52 +82,23 @@
 
 
 <style>
-
-/* figure{
-  float:left;
-} */
-
-/* * {
-  box-sizing: border-box;
-} */
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
+img{
+  width:333px;
+  height:500px;
 }
 
-/* Float four columns side by side */
-.column {
-  float: left;
-  width: 25%;
-  padding: 0 10px;
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
 }
 
-/* Remove extra left and right margins, due to padding */
-.row {margin: 0 -5px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
+.card{
+  border:2px solid green;
 }
 
-/* Responsive columns */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
 
-/* Style the counter cards */
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 16px;
-  text-align: center;
-  background-color: #f1f1f1;
-}
 
  
 </style>
