5747e08cb839587b99f99ccf6fb8d61210d6f34d
Chelsey McKinney <chelseymckinney@Chelseys-MacBook-Pro.local>
2019-11-16 08:22:49 -0600

    adding content to new files

---
+++ chels-portfolio/src/components/About.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="about-pg">
+        <h1>Hello I'm Chelsey!</h1>
+            <figure style= "float: right; margin: 4px 10px 0px 0px; ">
+            <img src="img/chelsey.jpg" style= "height:280px; width:250px;" alt="Chelsey at an event" />
+                <figcaption>
+                    Photo Credit: Jenessa White
+                </figcaption>
+            <!--add social media icons here-->
+            </figure>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <hr/>
+            <footer>Copyright Chelsey McKinney</footer>
+    </div>
+</template>
+
+
+
+
+<script></script>
+
+
+
+
+
+<style></style>
---
+++ chels-portfolio/src/components/Now.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="now-pg">
+        <h1>Now page</h1>
+        <h2>What is a Now Page?</h2>
+        <h3>A now page is a better way of explaining to others what you are currently working on </h3>
+
+        <h3>What I'm Working On Now</h3>
+        <h3>What I'm Reading Now</h3>
+        <h3>What I'm Listening To Now</h3>
+
+        <hr/>
+        <footer>Copyright Chelsey McKinney</footer>
+    </div>
+</template>
+
+
+
+
+
+<script></script>
+
+
+
+
+
+<style></style>
