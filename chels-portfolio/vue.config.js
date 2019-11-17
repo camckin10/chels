1c2f49078e119d5f0cd5a5f8e8d61c3df667dcaa
Chelsey McKinney <chelseymckinney@Chelseys-MacBook-Pro.local>
2019-11-17 16:15:29 -0600

    changing file structure for multi-pgs

--- chels-portfolio/vue.config.js
+++ chels-portfolio/vue.config.js
@@ -1,4 +1,35 @@
 // vue.config.js
 module.exports = {
-    publicPath: './'
+    pages: {
+        'index': {
+          entry: './src/pages/Home/main.js',
+          template: 'public/index.html',
+          title: 'Home'
+          //chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+        },
+        'about': {
+          entry: './src/pages/About/main.js',
+          template: 'public/index.html',
+          title: 'About'
+          //chunks: [ 'chunk-vendors', 'chunk-common', 'about' ]
+        }
+        ,'now': {
+            entry: './src/pages/Now/main.js',
+            template: 'public/index.html',
+            title: 'Now'
+            //chunks: [ 'chunk-vendors', 'chunk-common', 'about' ]
+          }, 
+          'work': {
+            entry: './src/pages/Work/main.js',
+            template: 'public/index.html',
+            title: 'Work'
+            //chunks: [ 'chunk-vendors', 'chunk-common', 'about' ]
+          },
+          'contact': {
+            entry: './src/pages/Contact/main.js',
+            template: 'public/index.html',
+            title: 'Contact'
+            //chunks: [ 'chunk-vendors', 'chunk-common', 'about' ]
+          }
+      }
 }
