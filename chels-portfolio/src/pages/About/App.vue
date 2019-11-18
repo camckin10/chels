78267a2f4bda03bf306739d712092dabfb7f9e44
Chelsey McKinney <chelseymckinney@Chelseys-MacBook-Pro.local>
2019-11-18 11:11:21 -0600

    navbar to about + now page + now pg. content

--- chels-portfolio/src/pages/About/App.vue
+++ chels-portfolio/src/pages/About/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <h1>About Page</h1>
+     <!-- The navigation menu -->
+    <div class="navbar">
+      <a class="active" href="home.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="now.html">Now</a>
+      <a href="work.html">Work</a>
+      <a href="contact.html">Contact</a>
+    </div>
+    <h1>About Me</h1>
       <div class="about-pg">
         <h1>Hello I'm Chelsey!</h1>
             <figure style= "float: right; margin: 4px 10px 0px 0px; ">
@@ -38,12 +46,58 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  background-color: #eee2dc;
+}
+
+/* Style the navigation menu */
+.navbar {
+  width: 100%;
+  background-color: #ac3b61;
+  overflow: auto;
+}
+
+/* Navigation links */
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  width: 17%; /* Four equal-width links. If you have two links, use 50%, and 33.33% for three links, etc.. */
+  text-align: center; /* If you want the text to be centered */
+}
+
+/* Add a background color on mouse-over */
+.navbar a:hover{ 
+  background-color: #ac3b61 ;
+}
+
+/* Style the current/active link */
+.navbar a.active {
+  background-color: #123c69;
+}
+
+/* Add responsiveness - on screens less than 500px, make the navigation links appear on top of each other, instead of next to each other */
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+    width: 100%;
+    text-align: left; /* If you want the text to be left-aligned on small screens */
+  }
+}
+
+h1{
+  color:#123c69;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #123c69;
   margin-top: 60px;
 }
 </style>
--- chels-portfolio/src/pages/Now/App.vue
+++ chels-portfolio/src/pages/Now/App.vue
@@ -1,16 +1,59 @@
 <template>
   <div id="app">
-    <h1>Now Page</h1>
+      <div class="navbar">
+        <a class="active" href="home.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="now.html">Now</a>
+        <a href="work.html">Work</a>
+        <a href="contact.html">Contact</a>
+      </div>
+    <h1>My Now Page</h1>
+    <h2>Inspired by Derek Siver's ,<a href="https://sivers.org/nowff">'now page movement'</a>, a now page answers the question, "What Are You Focused On Now?"</h2>
     <div class="now-pg">
-        <h1>Now page</h1>
-        <h2>What is a Now Page?</h2>
-        <h3>A now page is a better way of explaining to others what you are currently working on </h3>
-
+        <h3>Where I'm Working Now</h3>
+          <p>I'm a Full Stack Developer for The City of St.Paul.</p>
         <h3>What I'm Working On Now</h3>
-        <h3>What I'm Reading Now</h3>
-        <h3>What I'm Listening To Now</h3>
+          <p>I am doing the <a href="https://www.100daysofcode.com/">#100DaysOfCode Challenge</a> for a second time. 
+          I started the challenge in October 2019, and right now I am on Day 22 of the challenge.
+          I do daily updates on my <a href="https://twitter.com/TechChelsey">Twitter</a></p>
 
+        <h3>What I've Recently Read</h3>
+          <!--go back and add amazon/goodreads link-->
+          <ul>
+            <li>Washington Black by Esi Edugyan [currently reading]</li>
+            <li>The Book Of Night Women by Marlon James</li>
+            <li>The Nickel Boys by Colson Whitehead</li>
+            <li>Brief History of 7 Killings by Marlon James</li>
+          </ul>
+        <h3>What I'm Reading In The Future</h3>
+          <ul>
+            <li>The Year Of Yes by Shonda Rhimes</li>
+            <li>Barracoon by Zora Neal Hurston</li>
+            <li>My Sister, The Serial Killer by Oyinkan Braithwaite</li>
+            <li>John Crow's Devil by Marlon James</li>
+          </ul>
+        <h3>What I'm Listening To Now</h3>
+            <ul>
+              <li>Acrylic--Leikeli47</li>
+              <li>Fever--Megan Thee Stallion</li>
+              <li>Anger Management--Rico Nasty</li>
+              <li>Wash & Set--Leikeli47</li>
+              <li>Amala--Doja Cat</li>
+              <li>Initially go through the <a href="https://www.npr.org/2017/07/24/538387823/turning-the-tables-150-greatest-albums-made-by-women">150 Albums Made By Women(NPR)</a></li>
+            </ul>
+            <!--description list-->
+          <!-- <dl>
+            <dt>study</dt>
+            <dd>The devotion of time and attention to acquiring knowledge on an academic subject, especially by means of books</dd>
+            <dt>design</dt>
+            <dd>A plan or drawing produced to show the look and function or workings of a building, garment, or other object before it is built or made</dd>
+            <dd>Purpose, planning, or intention that exists or is thought to exist behind an action, fact, or material object</dd>
+            <dt>business</dt>
+            <dt>work</dt>
+            <dd>A person's regular occupation, profession, or trade</dd>
+          </dl> -->
         <hr/>
+        <h4>Updated November 18, 2019</h4>
         <footer>Copyright Chelsey McKinney</footer>
     </div>
   </div>
@@ -23,12 +66,59 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  background-color: #eee2dc;
+}
+
+/* Style the navigation menu */
+.navbar {
+  width: 100%;
+  background-color: #ac3b61;
+  overflow: auto;
+}
+
+/* Navigation links */
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  width: 17%; /* Four equal-width links. If you have two links, use 50%, and 33.33% for three links, etc.. */
+  text-align: center; /* If you want the text to be centered */
+}
+
+/* Add a background color on mouse-over */
+.navbar a:hover{ 
+  background-color: #ac3b61 ;
+}
+
+/* Style the current/active link */
+.navbar a.active {
+  background-color: #123c69;
+}
+
+/* Add responsiveness - on screens less than 500px, make the navigation links appear on top of each other, instead of next to each other */
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+    width: 100%;
+    text-align: left; /* If you want the text to be left-aligned on small screens */
+  }
+}
+
+h1{
+  color:#123c69;
+  text-align: center;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  //text-align: center;
+  color: #123c69;
   margin-top: 60px;
 }
 </style>
