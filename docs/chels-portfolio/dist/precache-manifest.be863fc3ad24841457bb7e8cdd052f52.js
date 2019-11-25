de08b2e031e2dd576419ce292bcfcc640c0c09e7
Chelsey McKinney <chelseymckinney2@gmail.com>
2019-11-25 14:25:21 -0600

    seeing which moving piece works

---
+++ docs/chels-portfolio/dist/precache-manifest.be863fc3ad24841457bb7e8cdd052f52.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85957fd3de64886c31fdb8d9dae6f282",
+    "url": "/chels/about.html"
+  },
+  {
+    "revision": "74f044b64ee6ca7ec5e51ceeb86b8614",
+    "url": "/chels/contact.html"
+  },
+  {
+    "revision": "c22d871f4d0e20f15142",
+    "url": "/chels/css/about.10dc3147.css"
+  },
+  {
+    "revision": "399ab96a6dd14a9ed971",
+    "url": "/chels/css/chunk-vendors.bd777184.css"
+  },
+  {
+    "revision": "ca786548812996be3792",
+    "url": "/chels/css/contact.3ea17097.css"
+  },
+  {
+    "revision": "7def654720cf74cf7fcd",
+    "url": "/chels/css/index.659f7775.css"
+  },
+  {
+    "revision": "f6e55dbe20bf5625f6fe",
+    "url": "/chels/css/now.cf6774b5.css"
+  },
+  {
+    "revision": "9715ffe657a9cd735c4f",
+    "url": "/chels/css/work.e0945a20.css"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/chels/fonts/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/chels/fonts/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/chels/fonts/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/chels/fonts/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/chels/fonts/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/chels/fonts/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/chels/fonts/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/chels/fonts/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/chels/fonts/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/chels/fonts/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/chels/fonts/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/chels/fonts/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "547012653e2d2ee1738287b958897d3b",
+    "url": "/chels/img/chelsey.jpg"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/chels/img/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/chels/img/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/chels/img/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "c4febfdfdeca0b08acfdba0d80a0ad7d",
+    "url": "/chels/img/laptop.jpg"
+  },
+  {
+    "revision": "a828485a39132ca7b00d4356b1a4da58",
+    "url": "/chels/index.html"
+  },
+  {
+    "revision": "c22d871f4d0e20f15142",
+    "url": "/chels/js/about.b3d131b1.js"
+  },
+  {
+    "revision": "399ab96a6dd14a9ed971",
+    "url": "/chels/js/chunk-vendors.c52752f6.js"
+  },
+  {
+    "revision": "ca786548812996be3792",
+    "url": "/chels/js/contact.17b117a6.js"
+  },
+  {
+    "revision": "7def654720cf74cf7fcd",
+    "url": "/chels/js/index.0e8897be.js"
+  },
+  {
+    "revision": "f6e55dbe20bf5625f6fe",
+    "url": "/chels/js/now.eca9b324.js"
+  },
+  {
+    "revision": "9715ffe657a9cd735c4f",
+    "url": "/chels/js/work.a614ea35.js"
+  },
+  {
+    "revision": "aea943b9bb048487af016a5f50f77b09",
+    "url": "/chels/manifest.json"
+  },
+  {
+    "revision": "3a0eb83f0d1e56a42883b3bd61ee3814",
+    "url": "/chels/now.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chels/robots.txt"
+  },
+  {
+    "revision": "f4e2c73a77c291d23c0d9b91f0e417e4",
+    "url": "/chels/work.html"
+  }
+]);
