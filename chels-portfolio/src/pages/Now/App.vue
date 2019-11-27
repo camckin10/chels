d3b9648be2b1fd416e7edc40d263eaf9bb10201b
Chelsey McKinney <chelseymckinney2@gmail.com>
2019-11-26 19:27:31 -0600

    re-doing now page layout

--- chels-portfolio/src/pages/Now/App.vue
+++ chels-portfolio/src/pages/Now/App.vue
@@ -8,58 +8,66 @@
         <a href="contact.html">Contact</a>
       </div>
       <hr/>
-    <h1>My Now Page</h1>
-    <h2>Inspired by Derek Siver's ,<a href="https://sivers.org/nowff">'now page movement'</a>, a now page answers the question, "What Are You Focused On Now?"</h2>
+    <h1>Now Page</h1>
     <div class="now-pg">
-        <h3>Where I'm Working Now</h3>
-          <p>I'm a Full Stack Developer for The City of St.Paul.</p>
-        <h3>What I'm Working On Now</h3>
-          <p>I am doing the <a href="https://www.100daysofcode.com/">#100DaysOfCode Challenge</a> for a second time. 
-          I started the challenge in October 2019, and right now I am on Day 22 of the challenge.
-          I do daily updates on my <a href="https://twitter.com/TechChelsey">Twitter</a></p>
-
-        <h3>What I'm Reading and What I've Recently Read</h3>
-          <!--go back and add amazon/goodreads link-->
-          <ul>
-            <li>Washington Black by Esi Edugyan [currently reading]</li>
-            <li>The Book Of Night Women by Marlon James [completed]</li>
-            <li>The Nickel Boys by Colson Whitehead [completed]</li>
-            <li>Brief History of 7 Killings by Marlon James[completed]</li>
-          </ul>
-        <h3>What I'm Reading Next</h3>
-         <ul>
-            <li>The Year Of Yes by Shonda Rhimes</li>
-            <li>Barracoon by Zora Neal Hurston</li>
-            <li>My Sister, The Serial Killer by Oyinkan Braithwaite</li>
-            <li>John Crow's Devil by Marlon James</li>
-          </ul>
-          <!-- <dl>
-            <dt>The Year Of Yes by Shonda Rhimes</dt>
-              <dd>A memoir of sorts; Rhimes spends the next year saying, 'Yes' to everything.</dd>
-              <br/>
-            <dt>Barracoon by Zora Neal Hurston</dt>
-              <dd>Tells the life story of Cudjo, an 86 year old slave who was interviewed by Zora Neale.</dd>
-              <br/>
-            <dt>My Sister, The Serial Killer by Oyinkan Braithwaite</dt>
-              <dd>A story of identical twins; One, a serial killer, The other, a cleaner for the murders.</dd>
-              <br/>
-            <dt>John Crow's Devil by Marlon James</dt>
-              <dd>Marlon James's debut novel about religion, redemption, and sexual obsession.</dd>  
-          </dl> -->
-        <h3>What I'm Listening To Now</h3>
-            <ul>
-              <li>Acrylic--Leikeli47</li>
-              <li>Fever--Megan Thee Stallion</li>
-              <li>Anger Management--Rico Nasty</li>
-              <li>Wash & Set--Leikeli47</li>
-              <li>Amala--Doja Cat</li>
-              <li>Initially go through the <a href="https://www.npr.org/2017/07/24/538387823/turning-the-tables-150-greatest-albums-made-by-women">150 Albums Made By Women(NPR)</a></li>
-            </ul>
+      <h3>Inspired by Derek Siver's ,<a href="https://sivers.org/nowff">'now page movement'</a>, a now page answers the question, "What Are You Focused On Now?"</h3>
+        <ul>
+          <li><b>Current Job:</b> Full Stack Developer for The City of St.Paul</li>
+          <li><b>For Fun:</b> I am doing the <a href="https://www.100daysofcode.com/">#100DaysOfCode Challenge</a> for a second time. 
+            I do daily updates on my <a href="https://twitter.com/TechChelsey">Twitter</a> page.</li>
+          <li><b>What I'm Reading:</b> "Red At The Bone" by Jacqueline Woodson & "Here Comes The Sun" by Nicole Dennis-Benn</li>
+          <li><b>Who I'm Listening To:</b> Leikeli47, Megan Thee Stallion, Rico Nasty, & Doja Cat</li>
+        </ul>
+          <hr/>
+      <!--Recent work section-->
+          <h2>Labors Of Love</h2>
+      <div class="grid-container">
+        <div class="card">
+          <div class="bg-img"></div>
+          <div class="content">
+           <h3>100 Days, Racism, and Starting Over</h3>
+            <p>What happens when a junior dev stops coding for 3 months? 
+              What really matters is what happens after the tech reprieve. Learn about how unplugging from technology allowed me to grow immensely, and how I regained my faith in programming by completing a coding challenge. 
+              Tech Talk given at Minnebar in April 2019. </p> 
+            <button class="purple-btn"><a href="">Read More</a></button> </div>
+        </div>
+        <div class="card">
+          <div class="bg-img"></div>
+          <div class="content">
+            <h3>Shit Black Parents Say Lorem Ipsum</h3>
+            <p>A lorem ipsum generator I made as an homage to Black Parents sayings. 
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+              The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) 
+              This project was made using HTML5, CSS3, and ExpressJS. </p> 
+            <button class="purple-btn"><a href="https://secret-temple-73467.herokuapp.com/">Read More</a></button></div>
+        </div>
+        <div class="card">
+          <div class="bg-img"></div>
+          <div class="content">
+            <h3>My Love Of Semicolons</h3>
+            <p>Last summer, I worked as an intern for a company called Software for Good.  
+              During my internship, I got the opportunity to write the "Theme Of The Week".This is when an employee from Software For Good takes time to write whats on their mind. 
+              For me, I wrote about my love of semicolons, and how programming has made me enjoy semicolons more.</p> 
+            <button class= "purple-btn"><a href="https://softwareforgood.com/my-love-of-semicolons/">Read More</a></button></div>
+        </div>
+        <div class="card">
+          <div class="bg-img"></div>
+          <div class="content">
+            <h3>Seshat Swap</h3>
+            <p>An app made during the 2019 'Hack The Gap' hackathon. 'Hack The Gap' is an all female hackathon.
+            “Seshat Swap,” is a community book sharing app that would promote authors of colors, and share books with other users of color. Worked in a group with Lanice Sims, Jenessa White, and Shanise Barona. 
+            Made using React and react strap. </p> 
+            <button class="purple-btn"><a href="https://agitated-swanson-0e797f.netlify.com/">Read More</a></button></div>
+          </div>
+       </div>
         <hr/>
-        <h4>Updated November 18, 2019</h4>
+        <h4>Updated November 26, 2019</h4>
         <footer>Copyright Chelsey McKinney</footer>
     </div>
   </div>
+
+
+
 </template>
 
 <script>
@@ -123,9 +131,60 @@ h1{
   text-align: center;
 }
 
+h2{
+  text-align: center;
+  font-size:25px;
+
+}
+
 h3{
   color:#ac3b61;
+  text-align: center;
+  font-size:25px;
+
 }
+
+ul{
+  list-style-type: circle;
+  font-size: 20px;
+}
+
+/*card stylings*/
+.purple-btn{
+   /* default for <button>, but useful for <a> */
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+
+  /* create a small space when buttons wrap on 2 lines */
+  /* margin: 2px 0; */
+  margin:10px;
+
+  /* invisible border (will be colored on hover/focus) */
+  border: solid 1px transparent #123c69;
+  border-radius: 4px;
+
+  /* size comes from text & padding (no width/height) */
+  padding: 0.5em 1em;
+
+  /* make sure colors have enough contrast! */
+  color: #ffffff;
+  background-color: #ac3b61;
+  Font-size:18px;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
+}
+
+.card{
+  /* blue violet for card borders*/
+  border:3px solid #123c69;
+}
+
+
 
 #app {
   font-family: 'Lato', sans-serif;
